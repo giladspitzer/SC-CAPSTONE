1cca73328c617f5de8cb5424af0b585b7bcf10a8
giladspitzer <52394657+giladspitzer@users.noreply.github.com>
2020-03-16 14:03:32 -0700

    with password reset -- good

--- app/forms.py
+++ app/forms.py
@@ -74,3 +74,12 @@ class CommentForm(FlaskForm):
     id = IntegerField()
     submit = SubmitField('Submit')
 
+class ResetPasswordRequestForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Request Password Reset')
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,8 @@ from flask import render_template, flash, redirect, url_for, request, jsonify, a
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, CommitForm, CommentForm
+from app.myemail import send_password_reset_email
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, CommitForm, CommentForm, ResetPasswordRequestForm, ResetPasswordForm
 from app.models import User, Assignment, AU_Relationships, Event, EU_Relationships, Commit, Section, Post, CommitComment, AssignmentNotes
 from datetime import datetime
 from app import general_url
@@ -131,7 +132,6 @@ def restricted(user, type):
 
 @app.route('/')
 @app.route('/None')
-
 def main():
     return redirect(url_for('index'))
 
@@ -201,6 +201,36 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_email(user)
+        flash('Check your email for the instructions to reset your password')
+        return redirect(url_for('login'))
+    return render_template('reset_password_request.html',
+                           title='Reset Password', form=form)
+
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = User.verify_reset_password_token(token)
+    if not user:
+        return redirect(url_for('index'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.session.commit()
+        flash('Your password has been reset.')
+        return redirect(url_for('login'))
+    return render_template('reset_password.html', form=form)
 
 
 @app.route('/user/<username>', methods=['GET', 'POST'])
