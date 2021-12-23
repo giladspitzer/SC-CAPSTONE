3a0c9bd1d1a61d05f21d2b4c3b98c15ff1cde2d8
Gilad Spitzer <52394657+giladspitzer@users.noreply.github.com>
2021-12-23 00:16:20 -0500

    Update configurator.py

--- configurator.py
+++ configurator.py
@@ -6,13 +6,12 @@ class Config(object):
     POSTS_PER_PAGE = 10
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    # SQLALCHEMY_DATABASE_URI = '***REMOVED***'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     os.environ['MAIL_SERVER'] = 'smtp.googlemail.com'
     os.environ['MAIL_PORT'] = '587'
     os.environ['MAIL_USE_TLS'] = '1'
     os.environ['MAIL_USERNAME'] = 'devgilad@gmail.com'
-    os.environ['MAIL_PASSWORD'] = current_app.config.get('MAIL_PASSWORD')
+    os.environ['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
